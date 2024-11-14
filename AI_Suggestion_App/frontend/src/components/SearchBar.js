f19eb01960e158ed91b9e8d82383e41cba5f9c85
Jehan Rodrigo <jehanrodrigo31@gmail.com>
2024-11-14 10:52:29 +0530

    Update SearchBar.js

--- AI_Suggestion_App/frontend/src/components/SearchBar.js
+++ AI_Suggestion_App/frontend/src/components/SearchBar.js
@@ -68,7 +68,7 @@ function SearchBar() {
             // Debounce API calls
             debounceTimeout.current = setTimeout(async () => {
                 try {
-                    const response = await axios.post('https://medicalassistantbackend-production.up.railway.app/', { 
+                    const response = await axios.post('https://medicalassistantbackend-production.up.railway.app', { 
                         input: currentLine 
                     });
                     
