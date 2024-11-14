9fe0c9d947881c0fc0730e711389a54dcf605ffa
Jehan Rodrigo <jehanrodrigo31@gmail.com>
2024-11-14 10:47:25 +0530

    Railway URL Added

--- AI_Suggestion_App/frontend/src/components/SearchBar.js
+++ AI_Suggestion_App/frontend/src/components/SearchBar.js
@@ -68,7 +68,7 @@ function SearchBar() {
             // Debounce API calls
             debounceTimeout.current = setTimeout(async () => {
                 try {
-                    const response = await axios.post('https://allied-andreana-jehan-rodrigo-b4f5bb76.koyeb.app/', { 
+                    const response = await axios.post('https://medicalassistantbackend-production.up.railway.app/', { 
                         input: currentLine 
                     });
                     
